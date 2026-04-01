8afa701f83012bc4612bc3f58cac666a470f8d6f
User34 <totipodaquire@gmail.com>
2026-04-01 11:45:39 -0500

    Add files via upload

---
+++ AutoPiano/License.bat
@@ -0,0 +1,30 @@
+@echo off
+title DeltaLove - Official License
+color 0D
+
+echo ============================================
+echo        DeltaLove - Official Script
+echo ============================================
+echo.
+echo  Created by: KDC (Kris Dev Club)
+echo.
+echo  Main Developer:
+echo     GitHub: 1uwu87 (user34)
+echo.
+echo  Description:
+echo     Este script fue desarrollado con el fin de
+echo     proporcionar ventajas equitativas usando AHK v2.0
+echo.
+echo  License:
+echo     Este software es propiedad de KDC.
+echo     No se permite copiar, modificar o redistribuir
+echo     sin autorizacion del creador.
+echo.
+echo  Status:
+echo     [OFICIAL]  [ESTABLE]
+echo.
+echo  FIRMA: DELTAUNDERLOVER
+echo 
+echo.
+echo ============================================
+pause
